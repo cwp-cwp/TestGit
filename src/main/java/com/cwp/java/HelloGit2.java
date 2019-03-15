884f79a9b4b4ad8146979dbdc8b2829899ecf6c6
cwp-cwp <1733459640@qq.com>
2019-03-15 23:32:52 +0800

    修改提交

--- src/main/java/com/cwp/java/HelloGit2.java
+++ src/main/java/com/cwp/java/HelloGit2.java
@@ -1,8 +1,15 @@
 package com.cwp.java;
 
+import org.junit.Test;
+
 public class HelloGit2 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git...............");
+	}
+	
+	@Test
+	public void test1(){
+		System.out.println("...................");
 	}
 }
